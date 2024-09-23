05607dc7f96d91b0ce4fa089cb41d9aecfefe8f8
DeLsonJabberwo <pdhl2006@gmail.com>
2024-09-23 14:19:39 -0500

    added vimtex plugin

--- lua/delson/lazy.lua
+++ lua/delson/lazy.lua
@@ -96,6 +96,15 @@ return require('lazy').setup({
       branch = "harpoon2",
       dependencies = { "nvim-lua/plenary.nvim" }
   },
+  {
+      "lervag/vimtex",
+      lazy = false,     -- we don't want to lazy load VimTeX
+      -- tag = "v2.15", -- uncomment to pin to a specific release
+      init = function()
+          -- VimTeX configuration goes here, e.g.
+          vim.g.vimtex_view_method = "zathura"
+      end
+  },
 
   'nvim-treesitter/playground',
   'mbbill/undotree',
